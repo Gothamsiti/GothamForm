bb06ed9a79ae3b0f3240fc56dd88cd9da4f1591d
Roberto Silvesttri <roberto@gotham.studio>
2026-03-03 12:33:11 +0100

    fix composable

--- src/runtime/components/Main.vue
+++ src/runtime/components/Main.vue
@@ -14,7 +14,7 @@ section.Form(ref="$el" :class="{submitting:_submitting,submitted:_submittedCompu
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed } from 'vue'
 
 const { blok, storyUuid } = defineProps(['blok', 'storyUuid'])
 const fields = ref(undefined)
