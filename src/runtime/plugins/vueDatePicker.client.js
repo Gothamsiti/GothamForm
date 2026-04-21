4c14e96e2c27b48c429b130708ed4ab2ac2ea0ea
Roberto Silvestri <roberto@gotham.studio>
2026-04-21 10:01:07 +0200

    restored css

--- src/runtime/plugins/vueDatePicker.client.js
+++ src/runtime/plugins/vueDatePicker.client.js
@@ -1,4 +1,4 @@
 import { defineNuxtPlugin } from '#app'
-
+import '@vuepic/vue-datepicker/dist/main.css'
 // VueDatePicker is loaded lazily by fieldDatePickerRange via defineAsyncComponent
 export default defineNuxtPlugin(() => {})
