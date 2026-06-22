9854effa997b3bc55f16d317b617b6a15b268439
Umberto <umberto@gotham.studio>
2026-06-22 11:34:37 +0200

    chore: pre-release changes

--- dist/runtime/components/fields/fieldFlexGroup.vue
+++ dist/runtime/components/fields/fieldFlexGroup.vue
@@ -2,7 +2,7 @@
 .fieldFlexGroup
     template(v-for="subfield of blok?.originalblok?.fields" :key="subfield._uid")
         StoryblokComponent( v-if="subfield.name" v-model:model="subfield.value" :blok="{component:subfield.component,originalblok:subfield}" :formSlug="formSlug" :field="subfield" @addEvalFunction="_addEvalFunction" v-editable="subfield")
-        StoryblokComponent( v-else :blok="{component:subfield.component,originalblok:subfield}" :formSlug="formSlug" :field="subfield" v-editable="subfield")
+        StoryblokComponent( v-else :blok="{component:subfield.component,originalblok:subfield}" :formSlug="formSlug" :field="subfield" @addEvalFunction="_addEvalFunction" v-editable="subfield")
 </template>
 
 <script setup>
